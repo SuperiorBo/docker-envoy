ff00661969aea2298bcb1e8aa4442f2ffc1e07f5
chobon <chobon@aliyun.com>
2023-12-21 16:44:15 +0800

    docker compose init

---
+++ docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo "Generating envoy.yaml config file..."
+cat /tmpl/envoy.yaml.tmpl | envsubst \$ENVOY_APP_API_ADDRESS,\$ENVOY_COURSE_API_ADDRESS > /etc/envoy.yaml
+
+echo "Starting Envoy..."
+/usr/local/bin/envoy -c /etc/envoy.yaml
